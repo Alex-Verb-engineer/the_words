3b04e4c355115c6169482b11eb4f814ff67d3806
Aleksandr Gusinskii <alex.verb.engineer@gmail.com>
2024-04-21 00:32:18 +0300

    Added avoided.txt, code reading enhancements

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # the_words
 Convert column of words to the string with commas
+Input file: 'input.txt'
+Output file: 'output.txt'
+Avoided words (column): 'avoided.txt'
--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-input_file = open('input.txt', 'r', encoding='utf-8')
-# input_string = input_file.readlines()
-# Интерпретатор запоминает положение курсора считывания после этого действия
+"""
+Программа, переводящая вертикальную колонку слов
+в горизонтальную через запятую
+входной файл input.txt
+выходной файл output.txt
+"""
 
-output_file = open('output.txt', 'w', encoding='utf-8')
+INPUT = 'input.txt'
+OUTPUT = 'output.txt'
+AVOIDED = 'avoided.txt'
 
-for line in input_file:
-    output_file.write(line[:-1] + ', ')
 
-output_file.close()
-input_file.close()
+def column_to_comma_string(input_file, output_file, avoided_file=None):
+    avoided = avoided_file.readlines()
+    for line in input_file:
+        if line not in avoided:
+            output_file.write(line[:-1] + ', ')
+
+
+if __name__ == '__main__':
+    input_file = open(INPUT, 'r', encoding='utf-8')
+    # input_string = input_file.readlines()
+    # Интерпретатор запоминает положение курсора считывания после этого
+    # действия
+
+    avoided_file = open(AVOIDED, 'r', encoding='utf-8')
+
+    output_file = open(OUTPUT, 'w', encoding='utf-8')
+
+    column_to_comma_string(input_file, output_file, avoided_file)
+
+    output_file.close()
+    input_file.close()
+    avoided_file.close()
