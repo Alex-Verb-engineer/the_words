ddb56742e349794ed3e704818cfd09f5e3c80665
Aleksandr Gusinskii <alex.verb.engineer@gmail.com>
2024-04-21 00:32:18 +0300

    Added an exception for missing avoided.txt

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-input_file = open('input.txt', 'r', encoding='utf-8')
-# input_string = input_file.readlines()
-# Интерпретатор запоминает положение курсора считывания после этого действия
+"""
+Программа, переводящая вертикальную колонку слов
+в горизонтальную через запятую
+входной файл input.txt
+выходной файл output.txt
+Опционально - файл с использованными словами avoided.txt
+"""
 
-output_file = open('output.txt', 'w', encoding='utf-8')
+INPUT = 'input.txt'
+OUTPUT = 'output.txt'
+AVOIDED = 'avoided.txt'
 
-for line in input_file:
-    output_file.write(line[:-1] + ', ')
 
-output_file.close()
-input_file.close()
+def column_to_comma_string(input_file, output_file, avoided_file=None):
+    if avoided_file:
+        avoided = avoided_file.readlines()
+    else:
+        avoided = []
+
+    for line in input_file:
+        if line not in avoided:
+            output_file.write(line[:-1] + ', ')
+
+
+if __name__ == '__main__':
+    input_file = open(INPUT, 'r', encoding='utf-8')
+    # input_string = input_file.readlines()
+    # Интерпретатор запоминает положение курсора считывания после этого
+    # действия
+    avoided_file = None
+
+    try:
+        avoided_file = open(AVOIDED, 'r', encoding='utf-8')
+    except FileNotFoundError:
+        pass
+
+    output_file = open(OUTPUT, 'w', encoding='utf-8')
+
+    column_to_comma_string(input_file, output_file, avoided_file)
+
+    output_file.close()
+    input_file.close()
+
+    if avoided_file:
+        avoided_file.close()
